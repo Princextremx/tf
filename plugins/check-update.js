d800146cf6816571f3eb8901c8c59ab794f161b2
Princextremx <pxtreme86@gmail.com>
2025-07-20 01:43:01 -0400

    Update check-update.js

--- plugins/check-update.js
+++ plugins/check-update.js
@@ -88,7 +88,7 @@ cmd({
         forwardingScore: 999,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-          newsletterJid: '120363372853772240@newsletter',
+          newsletterJid: '120363418161689316@newsletter',
           newsletterName: '𝗫𝗧𝗥𝗘𝗠𝗘-𝗫𝗠𝗗',
           serverMessageId: 143
         }

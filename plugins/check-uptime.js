757aefa93867f266283eaac7cd56a351b4ec699e
Princextremx <pxtreme86@gmail.com>
2025-07-20 01:43:28 -0400

    Update check-uptime.js

--- plugins/check-uptime.js
+++ plugins/check-uptime.js
@@ -103,7 +103,7 @@ async (conn, mek, m, { from, reply, args }) => {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363398101781980@newsletter',
+                    newsletterJid: '120363418161689316@newsletter',
                     newsletterName: '𝗫𝗧𝗥𝗘𝗠𝗘-𝗫𝗠𝗗',
                     serverMessageId: 143
                 }

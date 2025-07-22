b094baa23555afffceff72a2be3fb94f4ed50eb5
Princextremx <pxtreme86@gmail.com>
2025-07-22 12:53:08 -0400

    Update about.js

--- plugins/about.js
+++ plugins/about.js
@@ -5,7 +5,7 @@ cmd({
     alias: "dev",
     react: "👑",
     desc: "get owner dec",
-    category: "👑main",
+    category: "main",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {

84262a36d469882f3fb28ee99445b24eb28fc001
Princextremx <pxtreme86@gmail.com>
2025-07-26 03:16:28 -0400

    Update main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -5,28 +5,27 @@ const config = require('../config');
 
 cmd({
     pattern: "alive",
-    alias: ["bot", "online"],
+    alias: ["bo", "ob"],
     desc: "Check bot is alive or not",
-    category: "🍥 main",
-    react: "🌛",
+    category: "main",
+    react: "🌟",
     filename: __filename
 },
 async (conn, mek, m, { from, sender, reply }) => {
     try {
-        const status = `> *╭⭑━━⪨ 𝗫𝗧𝗥𝗘𝗠𝗘 𝗔𝗟𝗜𝗩𝗘 ⪩*
-> *┃*✨ *ʙᴏᴛ ɪs ᴀᴄᴛɪᴠᴇ & ᴏɴʟɪɴᴇ!*
-> *┃*
-> *┃*🧠 *ᴏᴡɴᴇʀ:* ${config.OWNER_NAME}
-> *┃*⚡ *ᴠᴇʀsɪᴏɴ:* 1.0.0
-> *┃*📝 *ᴘʀᴇғɪx:* [${config.PREFIX}]
-> *┃*📳 *ᴍᴏᴅᴇ:* [${config.MODE}]
-> *┃*💾 *ʀᴀᴍ:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${(os.totalmem() / 1024 / 1024).toFixed(2)}MB
-> *┃*🖥️ *ʜᴏsᴛ:* ${os.hostname()}
-> *┃*⌛ *ᴜᴘᴛɪᴍᴇ:* ${runtime(process.uptime())}
-> *╰⭑━━➤* *ʙʏ ᴘʀɪɴᴄᴇ xᴛʀᴇᴍᴇ*`;
+        const status = `╭─ 「 *\`𝐗𝐓𝐑𝐄𝐌𝐄-𝐗𝐌𝐃\`* 」
+│✨ *ʙᴏᴛ ɪs ᴀᴄᴛɪᴠᴇ & ᴏɴʟɪɴᴇ!*
+│🧠 *ᴏᴡɴᴇʀ:* ${config.OWNER_NAME}
+│⚡ *ᴠᴇʀsɪᴏɴ:* 1.0.0
+│📝 *ᴘʀᴇғɪx:* [${config.PREFIX}]
+│📳 *ᴍᴏᴅᴇ:* [${config.MODE}]
+│💾 *ʀᴀᴍ:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${(os.totalmem() / 1024 / 1024).toFixed(2)}MB
+│🖥️ *ʜᴏsᴛ:* ${os.hostname()}
+│⌛ *ᴜᴘᴛɪᴍᴇ:* ${runtime(process.uptime())}
+╰────────────────❍`;
 
         await conn.sendMessage(from, {
-            image: { url: `https://files.catbox.moe/kb7s9o.jpg` },
+            image: { url: `https://files.catbox.moe/p01dj2.jpg` },
             caption: status,
             contextInfo: {
                 mentionedJid: [m.sender],
@@ -34,7 +33,7 @@ async (conn, mek, m, { from, sender, reply }) => {
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
                     newsletterJid: '120363418161689316@newsletter',
-                    newsletterName: '𝗫𝗧𝗥𝗘𝗠𝗘-𝗫𝗠𝗗',
+                    newsletterName: '𝐗𝐓𝐑𝐄𝐌𝐄-𝐗𝐌𝐃',
                     serverMessageId: 143
                 }
             }

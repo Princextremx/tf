005a40e839fd17a1c98442e740bd720804e1530a
Princextremx <pxtreme86@gmail.com>
2025-07-26 02:32:18 -0400

    Update Audio.js

--- plugins/Audio.js
+++ plugins/Audio.js
@@ -8,7 +8,7 @@ const ytdl = require('ytdl-core');
 cmd({
   pattern: "audio",
   desc: "Convertit une vidéo en audio MP3",
-  category: "⚓ mp3",
+  category: "misc",
   filename: __filename,
   react: "🎧"
 }, async (m, { quoted, args, mime }) => {

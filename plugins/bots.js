12f11aa929d1c37a4e68ae7aaf0c85125369cedc
Princextremx <pxtreme86@gmail.com>
2025-07-29 18:57:48 -0400

    Update bots.js

--- plugins/bots.js
+++ plugins/bots.js
@@ -1,43 +1,45 @@
-const config = require('../config')
+const { cmd } = require('../command');
 const { runtime } = require('../lib/functions');
-const {cmd , commands} = require('../command')
+const config = require('../config');
+
 cmd({
     pattern: "bot",
-    alias: "bots","${config.PREFIX}",
-    react: "🤖",
-    desc: "get owner dec",
+    alias: ["bott", "xtreme"],
+    desc: "thanks to dev for helping",
     category: "misc",
+    react: "🤖",
     filename: __filename
 },
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-    const uptime = runtime(process.uptime());
-    const startTime = new Date(Date.now() - process.uptime() * 1000);
-let about = `╭─ 「 *\`BOT ONLINE\`* 」
-│꙳ *ʙᴏᴛ ɴᴀᴍᴇ* ↔ ᴍɪɴɪ xᴛʀᴇᴍᴇ
+async (conn, mek, m, { from }) => {
+    try {
+        const uptime = runtime(process.uptime());
+        const startTime = new Date(Date.now() - process.uptime() * 1000);
+        const about = `╭─ 「 *\`BOT ONLINE\`* 」
+│꙳ *ʙᴏᴛ ɴᴀᴍᴇ* ↔ xᴛʀᴇᴍᴇ xᴍᴅ
 │꙳ *sᴛᴀᴛᴜs* ↔ ᴏɴʟɪɴᴇ
 │꙳ *ᴀᴜᴛᴏ ʀᴇsᴛᴀʀᴛ* ↔ ᴏɴʟɪɴᴇ
 │꙳ *ʙᴏᴛ ʀᴜɴ* ↔ ${uptime}
 │꙳ *ᴅᴇᴠɪᴄᴇ ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛ*
 ╰────────────────❍
-> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴘʀɪɴᴄᴇ xᴛʀᴇᴍᴇ*`
-await conn.sendMessage(from, {
-    image: { url: 'https://files.catbox.moe/p01dj2.jpg' },
-    caption: about,
-    contextInfo: {
-        mentionedJid: [m.sender],
-        forwardingScore: 999,
-        isForwarded: true,
-        forwardedNewsletterMessageInfo: {
-            newsletterJid: '120363418161689316@newsletter', // ou ton JID actuel
-            newsletterName: '𝐗𝐓𝐑𝐄𝐌𝐄-𝐗𝐌𝐃',
-            serverMessageId: 143
-        }
-    }
-}, { quoted: mek })
+> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴘʀɪɴᴄᴇ xᴛʀᴇᴍᴇ*`;
 
-}catch(e){
-console.log(e)
-reply(`${e}`)
-}
-})
+        await conn.sendMessage(from, {
+            image: { url: 'https://files.catbox.moe/mry39g.jpg' },
+            caption: about,
+            contextInfo: {
+                mentionedJid: [m.sender],
+                forwardingScore: 1000,
+                isForwarded: true,
+                forwardedNewsletterMessageInfo: {
+                    newsletterJid: '120363418161689316@newsletter', // remplace avec ton vrai newsletterJid si besoin
+                    newsletterName: '𝐗𝐓𝐑𝐄𝐌𝐄-𝐗𝐌𝐃',
+                    serverMessageId: 143
+                }
+            }
+        }, { quoted: mek });
+
+    } catch (err) {
+        console.error("ThanksTo Error:", err);
+        await conn.sendMessage(from, { text: `Error: ${err.message}` }, { quoted: mek });
+    }
+});

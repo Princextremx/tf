9804797316e905daf0b12839175b2d4241e0927a
Princextremx <pxtreme86@gmail.com>
2025-07-29 18:47:34 -0400

    Create bots.js

---
+++ plugins/bots.js
@@ -0,0 +1,41 @@
+const config = require('../config')
+const { runtime } = require('../lib/functions');
+const {cmd , commands} = require('../command')
+cmd({
+    pattern: "about",
+    alias: "bots",
+    react: "🤖",
+    desc: "get owner dec",
+    category: "misc",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let about = `╭─ 「 *\`BOT ONLINE\`* 」
+│꙳ *ʙᴏᴛ ɴᴀᴍᴇ* ↔ ᴍɪɴɪ xᴛʀᴇᴍᴇ
+│꙳ *sᴛᴀᴛᴜs* ↔ ᴏɴʟɪɴᴇ
+│꙳ *ᴀᴜᴛᴏ ʀᴇsᴛᴀʀᴛ* ↔ ᴏɴʟɪɴᴇ
+│꙳ *ʙᴏᴛ ʀᴜɴ* ↔ ${uptime}
+│꙳ *ᴅᴇᴠɪᴄᴇ ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛ*
+╰────────────────❍
+> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴘʀɪɴᴄᴇ xᴛʀᴇᴍᴇ*`
+await conn.sendMessage(from, {
+    image: { url: 'https://files.catbox.moe/p01dj2.jpg' },
+    caption: about,
+    contextInfo: {
+        mentionedJid: [m.sender],
+        forwardingScore: 999,
+        isForwarded: true,
+        forwardedNewsletterMessageInfo: {
+            newsletterJid: '120363418161689316@newsletter', // ou ton JID actuel
+            newsletterName: '𝐗𝐓𝐑𝐄𝐌𝐄-𝐗𝐌𝐃',
+            serverMessageId: 143
+        }
+    }
+}, { quoted: mek })
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})

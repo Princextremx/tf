ccb38c2661f4ca8ab5c23a208e86d3f469023e17
Princextremx <pxtreme86@gmail.com>
2025-08-10 02:41:01 -0600

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -81,7 +81,7 @@ async (conn, mek, m, { from, quoted, sender, reply }) => {
         const responseTime = (end - start) / 1000;
         let ping = await conn.sendMessage(from , { text: '*ʜᴇʀᴇ ɪs ʏᴏᴜʀ ᴘɪɴɢ...*'  }, { quoted: mek } )
 
-        const text = `*${reactionEmoji} 𝐏๏፝֟ƞ̽g ${responseTime.toFixed(2)} 𝐌𝐒*`;
+        const text = `*${reactionEmoji} p๏ƞ͛ɠ ${responseTime.toFixed(2)} ɱʂ*`;
 
         await conn.sendMessage(from, {
             text,
